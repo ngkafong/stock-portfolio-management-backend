0b473deb5adc3400929727bfc45f3b410e627ab6
ngkafong <richardng1126@gmail.com>
2021-07-13 13:02:41 +0800

    update models.py according to ER diagram

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Numeric, ForeignKey
+from sqlalchemy import Column, Integer, String, Numeric, ForeignKey, Date
 from sqlalchemy.orm import relationship
 
 from database import Base
@@ -7,29 +7,46 @@ from database import Base
 class Portfolio(Base):
     __tablename__ = "portfolios"
 
-    id = Column(Integer, primary_key=True, index=True)
+    portfolio_id = Column(Integer, primary_key=True, index=True)
     title = Column(String, unique=True, index=True)
 
-    transactions = relationship('Transaction', back_populates="portfolio")
-    calculation_result_filename = Column(String)
+    portfolio_stocks = relationship('PortfolioStock', back_populates="portfolio")
+
+class Stock(Base):
+    __tablename__ = "stocks"
+
+    stock_symbol = Column(String, primary_key=True)
+
+    portfolio_stocks = relationship('PortfolioStock', back_populates="stock")
+    stock_close_prices = relationship('StockClosePrice')
+
+class PortfolioStock(Base):
+    __table__ = "portfolio_stocks"
+
+    portfolio_id = Column(Integer, ForeignKey('portfolios.id'), primary_key=True)
+    stock_symbol = Column(String, ForeignKey('stocks.id'), primary_key=True)
+
+    portfolio = relationship('Portfolio', back_populates="portfolio_stocks")
+    stock = relationship('Stock', back_populates="portfolio_stocks")
+    transactions = relationship('Transaction', back_populates="portfolio_stock")
 
 
 class Transaction(Base):
     __tablename__ = "transactions"
 
-    id = Column(Integer, primary_key=True, index=True)
+    transaction_id = Column(Integer, primary_key=True, index=True)
     portfolio_id = Column(Integer, ForeignKey('portfolios.id'))
+    stock_symbol = Column(String, ForeignKey('stocks.stock_symbol'))
+    transaction_date = Column(Date)
     action = Column(String)
-    stock = Column(String)
     shares = Column(Integer, default=0)
     value = Column(Numeric(10, 3), default=0)
+    fee = Column(Numeric(10, 3), default=0)
 
-    portfolio = relationship('Portfolio', back_populates="transactions")
 
+class StockClosePrice(Base):
+    __tablename__ = "stock_close_prices"
 
-class Stock(Base):
-    __tablename__ = "stocks"
+    stock_symbol = Column(String, ForeignKey('stock'), primary_key=True)
+    date = Column(Date, primary_key=True)
 
-    symbol = Column(String, primary_key=True)
-    data_filename = Column(String, nullable=True)
-    calculation_result_filename = Column(String, nullable=True)
