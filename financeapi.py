41011a85731c308050b71582f8bd6e71334a59fb
ngkafong <richardng1126@gmail.com>
2021-07-20 17:10:55 +0800

    bugfix to financeapi

--- financeapi.py
+++ financeapi.py
@@ -46,13 +46,14 @@ def update_stock_list(db: Session, stock_list: Optional[pd.DataFrame] = None):
 
 
 
-def update_stock_history(db: Session, stock_symbol: str):
+def update_stock_price_history(db: Session, stock_symbol: str):
 
   stock = yf.Ticker(stock_symbol)
   close_price = msft.history(period="max")\
+    .reset_index()\
     .rename(columns = { 'Close': 'close_price'})
 
-  def update_stock_history_daily(close_price_daily):
+  def update_stock_price_daily(close_price_daily):
     _close_price_daily = schemas.StockClosePrice(
       stock_symbol = stock_symbol,
       close_price = close_price_daily['close_price'],
@@ -61,6 +62,7 @@ def update_stock_history(db: Session, stock_symbol: str):
     db_stock_close_price = models.StockClosePrice(**_close_price_daily.dict())
     db.merge(db_stock_close_price)
 
+  close_price.apply(update_stock_price_daily, axis=1)
   db.commit()
   print('stock history updated', stock_symbol)
   return
