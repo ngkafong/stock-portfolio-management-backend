68683103180e53ee2fed4c25b2dc098724299198
Ng Ka Fong <richardng1126@gmail.com>
2021-07-22 20:59:41 +0800

    calculate.py completed

--- calculate.py
+++ calculate.py
@@ -1,3 +1,4 @@
+from pandas.core.reshape.merge import merge
 from sqlalchemy.orm import Session
 import models
 from operator import itemgetter
@@ -6,6 +7,8 @@ import numpy as np
 import financeapi
 from typing import List
 from datetime import timedelta
+
+
 
 def transaction_record_to_account_movement(transaction: dict):
 
@@ -30,7 +33,7 @@ def transaction_record_to_account_movement(transaction: dict):
 
   if action == 'dividend-share':
       share_change = shares
-      cost_cahgne = fee
+      cost_change = fee
 
 
   return {
@@ -39,6 +42,8 @@ def transaction_record_to_account_movement(transaction: dict):
     'share_change': share_change,
     'cost_change': cost_change,
   }
+
+
 
 def stock_movements_to_balance(movements_df: pd.DataFrame):
 
@@ -56,35 +61,29 @@ def stock_movements_to_balance(movements_df: pd.DataFrame):
 
   return balance_df
 
-def merge_balance_with_prices(balance_df: pd.DataFrame, stock_price_df: pd.DataFrame):
 
-  return pd.merge_ordered(
-    balance_df, stock_price_df,
-    on='date',
-    fill_method='ffill'
-  ).dropna()
 
-def calculate_stock_statistics(balance_price_df: pd.DataFrame):
+def calculate_return_statistics(asset_df: pd.DataFrame):
 
-  # add column ['market_value', 'net_profit', 'daily_profit', 'daily_profit_percentage',
+  # input: df with column [ 'date', 'market_value', 'cost', ... ]
+  # output: df with column [ ...input_colums, 'net_profit', 'daily_profit', 'daily_profit_percentage',
   # 'return_rate', 'money_weighted_return_rate', 'time_weighted_return_rate']
+
 
   def calculate_adjusted_cost(df):
     # calculate adjusted cost according to formula of money weighted return rate
     start_date = df['date'].iloc[0]
-    df['cash_flow'] = np.diff(resultDF['cost'], prepend=0)
+    cash_flow = np.diff(df['cost'], prepend=0)
 
     def calculate_adjusted_cost_row(row):
-      sub_df = df.iloc[:row.name+1]
-      weight = (row.date - sub_df['date'] + timedelta(days=1)) / (row.date - start_date + timedelta(days=1))
-      return (sub_df['cash_flow'] * weight).sum()
+      weight = (row.date - df['date'].iloc[:row.name+1] + timedelta(days=1)) / (row.date - start_date + timedelta(days=1))
+      return (cash_flow[:row.name+1] * weight).sum()
 
     result = df.apply(calculate_adjusted_cost_row, axis=1)
     df.drop(columns='cash_flow')
     return result
 
-  result_df = result_df.copy()
-  result_df['market_value'] = result_df['price'] * result_df['share']
+  result_df = asset_df.copy()
   result_df['net_profit'] = result_df['market_value'] - result_df['cost']
   result_df['daily_profit'] = np.diff(result_df['net_profit'], prepend=0)
   result_df['daily_profit_percentage'] = result_df['daily_profit'] / result_df['cost']
@@ -95,7 +94,9 @@ def calculate_stock_statistics(balance_price_df: pd.DataFrame):
   return result_df
 
 
+
 def calculate_portfolio_stock(transactions: List[dict], stock_prices: List[dict]):
+
   account_movements_df = pd.DataFrame(
     map(transaction_record_to_account_movement, transactions)
   )
@@ -103,22 +104,30 @@ def calculate_portfolio_stock(transactions: List[dict], stock_prices: List[dict]
   balance_df = stock_movements_to_balance(account_movements_df)
   stock_price_df = pd.DataFrame(stock_prices)[['date', 'price']].sort_values('date')
 
-  balance_price_df = merge_balance_with_prices(balance_df, stock_price_df)
+  balance_price_df = pd.merge_ordered(
+    balance_df, stock_price_df,
+    on='date',
+    fill_method='ffill'
+  ).dropna()
 
-  result_df = calculate_stock_statistics(balance_price_df)
+  balance_price_df['market_value'] = balance_price_df['share'] * balance_price_df['price']
+
+  result_df = calculate_return_statistics(balance_price_df)
 
   return result_df.dict()
+
 
 
 def get_portfolio_stock_calculation_result(portfolio_id: int, stock_symbol: str, db: Session):
 
   transactions = db.query(models.PortfolioStock.transactions)\
     .filter(
-      models.PortfolioStock.portfolio_id == portfolio_id and
-      models.PortfolioStock.stock_symbol == stock_symbol
+      (models.PortfolioStock.portfolio_id == portfolio_id) &
+      (models.PortfolioStock.stock_symbol == stock_symbol)
     )\
     .all()
 
+  # fetch from api if database contain no price data
   if ( db.query(models.StockPrice)
     .filter(models.StockPrice.stock_symbol == stock_symbol)
     .first()
@@ -133,41 +142,34 @@ def get_portfolio_stock_calculation_result(portfolio_id: int, stock_symbol: str,
   return calculate_portfolio_stock(transactions, stock_prices)
 
 
+
 def calculate_multiple_assets(assets_result: dict):
 
-  # combine result of multiple stocks/portfolio to one single result
+  # input: { asset_1: asset_1_result, asset_2: asset_2_result }
+  # output: combined_asset_result [{date, market_value, cost, ...}, ...]
 
-  def merge_assets_result_to_df(assets_result: List[dict]):
-
-    return pd.concat(
-      [
-        pd.DataFrame(asset_result)[['date', 'cost', 'market_value']].set_index('date')
-        for asset_result in assets_result.values()
-      ],
-      axis=1,
-      keys=assets_result.keys()
-    ).reset_index()
-
+  merged_df = pd.concat(
+    [
+      pd.DataFrame(asset_result)[['date', 'cost', 'market_value']].set_index('date')
+      for asset_result in assets_result.values()
+    ],
+    axis=1,
+    keys=assets_result.keys()
+  ).reset_index()
 
 
-  def calculate_assets_statistics(assets_df: pd.DataFrame):
+  merged_df['cost'] = merged_df.xs('cost', axis=1, level=1).sum(axis=1)
+  merged_df['market_value'] = merged_df.xs('market_value', axis=1, level=1).sum(axis=1)
 
-    result_df = assets_df.copy()
-    result_df['cost'] =
+  result_df = calculate_return_statistics(merged_df[['date', 'cost', 'market_value']])
 
-    return result_df
-
-
-  assets_df = merge_assets_result_to_df(assets_result)
-  result_df = calculate_assets_statistics(assets_df)
-
-  return result.to_dict('records')
+  return result_df.to_dict('records')
 
 
 
 def get_portfolio_calculation_result(portfolio_id: int, db: Session):
 
-  portfolio_stocks = db.Query(models.Portfolio.portfolio_stocks)\
+  portfolio_stocks = db.query(models.Portfolio.portfolio_stocks)\
     .filter(models.Portfolio.portfolio_id == portfolio_id)\
     .all()
 
@@ -185,4 +187,24 @@ def get_portfolio_calculation_result(portfolio_id: int, db: Session):
   return {
     portfolio_result: portfolio_result,
     portfoliio_stocks_result: portfoliio_stocks_result
+  }
+
+
+def get_overall_calculation_result(db: Session):
+
+  portfolios = db.query(models.Portfolio).all()
+
+  portfolios_result = {
+    portfolio.portfolio_id: get_portfolio_calculation_result(
+      portfolio.portfolio_id,
+      db
+    ).portfolio_result
+    for portfolio in portfolios
+  }
+
+  overall_result = calculate_multiple_assets(portfolios_result)
+
+  return {
+    overall_result: overall_result,
+    portfolios_result: portfolios_result
   }
