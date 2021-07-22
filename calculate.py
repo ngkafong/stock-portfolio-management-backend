54b3cb00c638ad5849353f164a13f0c5b355b879
ngkafong <richardng1126@gmail.com>
2021-07-22 16:16:53 +0800

    checkpoint before refractor calculate_stock_statistics to calculate_return_statistics(balance_df)

--- calculate.py
+++ calculate.py
@@ -58,14 +58,13 @@ def stock_movements_to_balance(movements_df: pd.DataFrame):
 
 def merge_balance_with_prices(balance_df: pd.DataFrame, stock_price_df: pd.DataFrame):
 
-  return balance_df\
-    .merge(stock_price_df, how='outer', on='date')\
-    .fillna(method='ffill')\
-    .dropna()\
-    .sort_values('date')\
-    .reset_index(drop=True)
+  return pd.merge_ordered(
+    balance_df, stock_price_df,
+    on='date',
+    fill_method='ffill'
+  ).dropna()
 
-def calculate_profit_statistics(balance_price_df: pd.DataFrame):
+def calculate_stock_statistics(balance_price_df: pd.DataFrame):
 
   # add column ['market_value', 'net_profit', 'daily_profit', 'daily_profit_percentage',
   # 'return_rate', 'money_weighted_return_rate', 'time_weighted_return_rate']
@@ -102,13 +101,13 @@ def calculate_portfolio_stock(transactions: List[dict], stock_prices: List[dict]
   )
 
   balance_df = stock_movements_to_balance(account_movements_df)
-  stock_price_df = pd.DataFrame(stock_prices)[['date', 'price']]
+  stock_price_df = pd.DataFrame(stock_prices)[['date', 'price']].sort_values('date')
 
   balance_price_df = merge_balance_with_prices(balance_df, stock_price_df)
 
-  result_df = calculate_profit_statistics(balance_price_df)
+  result_df = calculate_stock_statistics(balance_price_df)
 
-  return result_df.to_dict('records')
+  return result_df.dict()
 
 
 def get_portfolio_stock_calculation_result(portfolio_id: int, stock_symbol: str, db: Session):
@@ -133,22 +132,55 @@ def get_portfolio_stock_calculation_result(portfolio_id: int, stock_symbol: str,
 
   return calculate_portfolio_stock(transactions, stock_prices)
 
+
+def calculate_multiple_assets(assets_result: dict):
+
+  # combine result of multiple stocks/portfolio to one single result
+
+  def merge_assets_result_to_df(assets_result: List[dict]):
+
+    return pd.concat(
+      [
+        pd.DataFrame(asset_result)[['date', 'cost', 'market_value']].set_index('date')
+        for asset_result in assets_result.values()
+      ],
+      axis=1,
+      keys=assets_result.keys()
+    ).reset_index()
+
+
+
+  def calculate_assets_statistics(assets_df: pd.DataFrame):
+
+    result_df = assets_df.copy()
+    result_df['cost'] =
+
+    return result_df
+
+
+  assets_df = merge_assets_result_to_df(assets_result)
+  result_df = calculate_assets_statistics(assets_df)
+
+  return result.to_dict('records')
+
+
+
 def get_portfolio_calculation_result(portfolio_id: int, db: Session):
 
   portfolio_stocks = db.Query(models.Portfolio.portfolio_stocks)\
     .filter(models.Portfolio.portfolio_id == portfolio_id)\
     .all()
 
-  portfoliio_stocks_result = [
-    get_portfolio_stock_calculation_result(
+  portfoliio_stocks_result = {
+    portfolio_stock.stock_symbol: get_portfolio_stock_calculation_result(
       portfolio_stock.portfolio_id,
       portfolio_stock.stock,
       db
     )
     for portfolio_stock in portfolio_stocks
-  ]
+  }
 
-  portfolio_result = calculate_portfolio(portfoliio_stocks_result)
+  portfolio_result = calculate_multiple_assets(portfoliio_stocks_result)
 
   return {
     portfolio_result: portfolio_result,
