551c61e63376322352c8440622072a71f175b8c7
Ng Ka Fong <richardng1126@gmail.com>
2021-08-04 22:19:13 +0800

    fix 'overall' rounding

--- calculate.py
+++ calculate.py
@@ -255,7 +255,7 @@ def get_overall_calculation_result(db: Session):
     for portfolio in portfolios
   }
 
-  overall_result = calculate_multiple_assets(portfolios_result)
+  overall_result = calculate_multiple_assets(portfolios_result).round(4).to_dict('list')
 
   return {
     "overall_result": overall_result,
