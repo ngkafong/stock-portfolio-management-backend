0056761e85520cb65f8e2e6859815a51a1a93297
Ng Ka Fong <richardng1126@gmail.com>
2021-08-04 21:28:10 +0800

    re-enable auto refresh stock data

--- main.py
+++ main.py
@@ -35,12 +35,12 @@ def get_db():
     finally:
         db.close()
 
-# @app.on_event("startup")
-# @repeat_every(seconds=60 * 60 * 3)
-# def update_all_stock_history():
-#     db = SessionLocal()
-#     financeapi.update_all_stock_price_history(db)
-#     db.close()
+@app.on_event("startup")
+@repeat_every(seconds=60 * 60 * 3)
+def update_all_stock_history():
+    db = SessionLocal()
+    financeapi.update_all_stock_price_history(db)
+    db.close()
 
 @app.get("/")
 def read_root():
